5527709f6082e5dbfbfed5f89d386e9c3a2ad3e1
Hakan AKSUMAN <h.aksuman@gmail.com>
2024-04-19 13:41:16 +0200

    Grid component implemented

---
+++ spfx/src/controls/components/grid/Grid.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import styles from "./Grid.module.scss";
+
+interface IGridProps {
+  container?: boolean;
+  item?: boolean;
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+  spacing?: number;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const Grid: React.FunctionComponent<IGridProps> = (props) => {
+  const { container, item, xs, sm, md, lg, xl, spacing, className, children } =
+    props;
+
+  const classList = [styles.customGrid];
+
+  if (className) {
+    classList.push(className);
+  }
+
+  if (container) {
+    classList.push(styles.container);
+    if (spacing) {
+      classList.push(styles[`spacing-${spacing}`]);
+    }
+  }
+
+  if (item) {
+    classList.push(styles.item);
+    if (xs) classList.push(styles[`xs-${xs}`]);
+    if (sm) classList.push(styles[`sm-${sm}`]);
+    if (md) classList.push(styles[`md-${md}`]);
+    if (lg) classList.push(styles[`lg-${lg}`]);
+    if (xl) classList.push(styles[`xl-${xl}`]);
+  }
+
+  return <div className={classList.join(" ")}>{children}</div>;
+};
--- spfx/src/webparts/componentTest/components/ComponentTest.tsx
+++ spfx/src/webparts/componentTest/components/ComponentTest.tsx
@@ -1,73 +1,147 @@
-import * as React from 'react';
-import styles from './ComponentTest.module.scss';
-import { IComponentTestProps } from './IComponentTestProps';
-import { escape } from '@microsoft/sp-lodash-subset';
-import { AddNewToolbox, IAddNewToolboxItem, ListItem, Plugins } from '@src/controls';
-import { Icon } from '@fluentui/react';
-import Base64Image from '@src/controls/components/base64Image/Base64Image';
+import * as React from "react";
+import styles from "./ComponentTest.module.scss";
+import { IComponentTestProps } from "./IComponentTestProps";
+import { escape } from "@microsoft/sp-lodash-subset";
+import {
+  AddNewToolbox,
+  Autocomplete,
+  IAddNewToolboxItem,
+  ListItem,
+  Plugins,
+} from "@src/controls";
+import { Icon } from "@fluentui/react";
+import Base64Image from "@src/controls/components/base64Image/Base64Image";
+import { Grid } from "@src/controls/components/grid/Grid";
 
-export default class ComponentTest extends React.Component<IComponentTestProps, {}> {
+export default class ComponentTest extends React.Component<
+  IComponentTestProps,
+  {}
+> {
   public render(): React.ReactElement<IComponentTestProps> {
-
-
     return (
       <>
         <AddNewToolbox
           onItemClick={(item) => {
             Plugins.AlertDialog.open({
-              content: "Das hast du geklickt: " + item.title
-            })
+              content: "Das hast du geklickt: " + item.title,
+            });
           }}
           items={[{ key: "bla", title: "Deine Oma", icon: "Add" }]}
         />
-
         Das hier sollte immer sichtbar sein:
-
         <AddNewToolbox
           hoverOnly={false}
           onItemClick={(item) => {
             Plugins.AlertDialog.open({
-              content: "Das hast du geklickt: " + item.title
-            })
+              content: "Das hast du geklickt: " + item.title,
+            });
           }}
           items={[
             { key: "bla1", title: "Deine Oma", icon: "Add" },
             { key: "bla2", title: "Deine Oma2", icon: "Edit" },
-            { key: "bla3", title: "Delete", icon: "Delete" }
+            { key: "bla3", title: "Delete", icon: "Delete" },
           ]}
         />
-
-        <div>
-          Custom Rendering
-        </div>
-
+        <div>Custom Rendering</div>
         <AddNewToolbox
           hoverOnly={false}
           onItemClick={(item) => {
             Plugins.AlertDialog.open({
-              content: "Das hast du geklickt: " + item.title
-            })
+              content: "Das hast du geklickt: " + item.title,
+            });
           }}
           items={[
             { key: "bla1", title: "Deine Oma", icon: "Add" },
             { key: "bla2", title: "Deine Oma2", icon: "Edit" },
-            { key: "bla3", title: "Delete", icon: "Delete" }
+            { key: "bla3", title: "Delete", icon: "Delete" },
           ]}
           onRenderItem={(item: IAddNewToolboxItem): JSX.Element => {
-            return <ListItem>
-              <Icon iconName={item.icon} />
-              {item.title}
-            </ListItem>
+            return (
+              <ListItem>
+                <Icon iconName={item.icon} />
+                {item.title}
+              </ListItem>
+            );
           }}
         />
-
         <h3>BASE64 IMAGE</h3>
         <Base64Image
-          base64String=''
+          base64String=""
           onChange={(newUrl: string) => {
             console.log(newUrl);
           }}
         />
+        <div
+          style={{
+            width: "100%",
+            backgroundColor: "red",
+            padding: "8px",
+          }}
+        >
+          <h1>Responsive Grid Layout</h1>
+          {/* Grid container with default spacing */}
+          <Grid container spacing={2}>
+            <Grid item xs={8} sm={6} md={4} lg={3}>
+              <div style={{ backgroundColor: "#ffcccc", padding: "10px" }}>
+                Item 1
+              </div>
+            </Grid>
+            <Grid item xs={4} sm={6} md={4} lg={3}>
+              <div style={{ backgroundColor: "#ccffcc", padding: "10px" }}>
+                Item 2
+              </div>
+            </Grid>
+            <Grid item xs={8} sm={6} md={4} lg={3}>
+              <div style={{ backgroundColor: "#ccccff", padding: "10px" }}>
+                Item 3
+              </div>
+            </Grid>
+            <Grid item xs={4} sm={6} md={4} lg={3}>
+              <div style={{ backgroundColor: "#ffffcc", padding: "10px" }}>
+                Item 4
+              </div>
+            </Grid>
+          </Grid>
+
+          <h2>Nested Grids</h2>
+          {/* Nested grids with different spacing */}
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8}>
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={6}>
+                  <div style={{ backgroundColor: "#ffcccc", padding: "10px" }}>
+                    Nested Item 1
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <div style={{ backgroundColor: "#ccffcc", padding: "10px" }}>
+                    Nested Item 2
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <div style={{ backgroundColor: "#ccccff", padding: "10px" }}>
+                Main Item 2
+              </div>
+            </Grid>
+          </Grid>
+
+          <h2>Dynamic Item Sizes</h2>
+          {/* Grid with dynamic item sizes */}
+          <Grid container spacing={4}>
+            <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
+              <div style={{ backgroundColor: "#ffffcc", padding: "10px" }}>
+                Wide Range Item
+              </div>
+            </Grid>
+            <Grid item xs={6} sm={8} md={9} lg={10} xl={11}>
+              <div style={{ backgroundColor: "#ffccff", padding: "10px" }}>
+                Large Item
+              </div>
+            </Grid>
+          </Grid>
+        </div>
       </>
     );
   }
